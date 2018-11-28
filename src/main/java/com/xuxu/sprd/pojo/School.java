447879591f1f82913575651e0e5951b31a5218f9
martea <1642485710@qq.com>
2018-11-28 11:34:32 +0800

    初始化顺序是beanNameAware-->beanFactoryAware,测试一下什么时候调用applicationContextAware，遗留问题循环遍历已经初始化完成的bean。

--- src/main/java/com/xuxu/sprd/pojo/School.java
+++ src/main/java/com/xuxu/sprd/pojo/School.java
@@ -3,24 +3,41 @@ package com.xuxu.sprd.pojo;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Created by martea on 2018/11/27.
  */
-public class School implements ApplicationContextAware, BeanFactoryAware, BeanNameAware{
+public class School implements /*ApplicationContextAware*/ BeanFactoryAware, BeanNameAware{
 
     private String beanId;
 
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("beanFactory"+beanFactory);
-    }
+    private DefaultListableBeanFactory factory;
 
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("applicationContext="+applicationContext);
+    private ClassPathXmlApplicationContext context;
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.factory = (DefaultListableBeanFactory) beanFactory;
+        System.out.println("applicationContext contains beans like : "+Arrays.asList(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(factory,Object.class)));
+        System.out.println("beanFactory contains beanDefinitions like : "+ Arrays.asList(factory.getBeanDefinitionNames()));
+        for (String s : factory.getBeanDefinitionNames()){
+            factory.getBeanDefinition(s);
+        }
     }
+//
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        this.context = (ClassPathXmlApplicationContext)applicationContext;
+//        System.out.println("getBeanDefinitionNames = "+Arrays.asList(context.getBeanDefinitionNames()));
+//        System.out.println("applicationContext="+applicationContext);
+//        System.out.println("applicationContext contains beans like : "+Arrays.asList(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(((ClassPathXmlApplicationContext) applicationContext).getBeanFactory(),Object.class)));
+//    }
 
     public void setBeanName(String name) {
         this.beanId = "beanName="+name;
@@ -32,5 +49,21 @@ public class School implements ApplicationContextAware, BeanFactoryAware, BeanNa
 
     public void setBeanId(String beanId) {
         this.beanId = beanId;
+    }
+
+    public BeanFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(DefaultListableBeanFactory factory) {
+        this.factory = factory;
+    }
+
+    public ApplicationContext getContext() {
+        return context;
+    }
+
+    public void setContext(ClassPathXmlApplicationContext context) {
+        this.context = context;
     }
 }
